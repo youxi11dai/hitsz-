f80c129261d6bed7552e93af86a0acabd7bd6bd5
unknown <unknown@example.com>
2021-03-14 20:24:18 +0800

    Initial Commit

---
+++ pages/search_input/index.js
@@ -0,0 +1,77 @@
+// pages/search_input/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabs:[
+      {
+        id: 0,
+        value: "特殊物品",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "一般物品",
+        isActive: false
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

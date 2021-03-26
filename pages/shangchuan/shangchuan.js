5d00eeb86399d42ffd4d9e2e3e47605c7cc6f718
unknown <unknown@example.com>
2021-03-26 23:53:22 +0800

    1.0.2

--- pages/shangchuan/shangchuan.js
+++ pages/shangchuan/shangchuan.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    scimg:[]
+    scimg:[],
+    name:'',
+    description:'',
+    conversation:'',
+    shifou:false
   },
 
   /**
@@ -64,18 +68,130 @@ Page({
 
   },
 
-  submit(){
-    
+  gaininformation1(e){
+    this.setData({
+      name:e.detail.value
+    })
   },
 
-  handleChooseImg(){
+  gaininformation2(e){
+    this.setData({
+      description:e.detail.value
+    })
+  },
+
+  gaininformation3(e){
+    this.setData({
+      conversation:e.detail.value
+    })
+  },
+
+  shifou(e){
+    const shifou=this.data.shifou;
+    this.setData({
+      shifou:!shifou
+    })
+  },
+
+  delete(e){
+    const {index}=e.currentTarget.dataset;
+    let {scimg}=this.data;
+    scimg.splice(index,1);
+    this.setData({
+      scimg
+    })
+  },
+
+  //选择图片
+  handleChooseImg(e){
     wx:wx.chooseImage({
-      count: 9,
+      count: 3,
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
       success: (result) => {
         this.setData({
-          scimg:[...this.data.img,...result.tempFilePaths]
+          scimg:[...this.data.scimg,...result.tempFilePaths]
+        })
+      }
+    })
+  },
+
+  tijiao(){
+    //设置时间戳
+    var timestamp=Date.parse(new Date());
+
+    //获取数据
+    const db=wx.cloud.database();
+    const name=this.data.name;
+    const description=this.data.description;
+    const conversation=this.data.conversation;
+    const shifou=this.data.shifou;
+    const {scimg}=this.data;
+    
+    //判断物品名称是否为空
+    if (!name.trim()) {
+      wx.showToast({
+        title: '请输入物品名称',
+        icon: 'none',
+        mask: true
+      });
+      return;
+    }
+
+    //判断联系方式或放置地点是否为空
+    if (!conversation.trim()) {
+      wx.showToast({
+        title: '请输入您的联系方式',
+        icon: 'none',
+        mask: true
+      });
+      return;
+    }
+
+    //判断上传的图片集合是否为空
+
+    wx.showLoading({
+      title:"正在上传",
+      mask:true
+    })
+    
+    if(scimg){
+      scimg.forEach((v,i)=>{
+        const cloudpath='lost'+timestamp;
+        wx.cloud.uploadFile({
+          cloudPath:cloudpath+i,
+          filePath:scimg[i],
+          success:(res)=>{
+            console.log(res)
+          }
+        })
+      })
+    }
+
+    //上传物品的相关信息
+    db.collection('lost').add({
+      data:{
+        name:name,
+        description:description,
+        conversation:conversation,
+        shifou:shifou,
+        scimg:scimg
+      },
+      success:function(res){
+        wx.hideLoading();
+        wx.showToast({
+          title: '上传成功',
+          mask:true
+        })
+        this.setData({
+          scimg:[],
+          name:'',
+          conversation:'',
+          description:'',
+          shifou:false
+        });
+        wx.navigateBack({
+          delta: 1,
         })
       }
     })
